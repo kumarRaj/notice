e96df4d7b12abe1bb4828bffb5d88487d0f6345a
Raj <rajkum@thoughtworks.com>
2013-11-12 16:53:39 +0530

    retrieving data from file done

--- host.js
+++
@@ -1,7 +0,0 @@
-var message = {};
-message.sender = "Kavita";
-message.date = new Date(2001,1,2);
-message.text = "Hi, Welcome to this service";
-
-var repository = [];
-repository.push(message);
--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-var showOutput = function(request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	inputUrl = request.url;
-	id = require('url').parse(inputUrl,true);
-	path = id.pathname.substr(1);
-	if(path == "add2")
-  		response.write(lib.sum2(id.query).toString());
-  	if(path == "accumulate"){
-  		sum = lib.accumulate(id.query,sum);
-      response.write(sum.toString());
-  		// response.write('Accumulation ' + sum);
-  	}
-  	if(path == "reset")
-  		sum = 0;
-  	response.end();
+/* --- Prep ---- */
+var fs = require('fs');
+var http = require('http');
+// var notice1 = {};
+// notice1.sender = "Kavita";
+// notice1.date = new Date(2001,1,2);
+// notice1.subject = "Welcome";
+// notice1.message = "Hi, Welcome to this service";
+
+// var repository = [];
+// repository.push(notice1);
+// var notice2 = {};
+
+// notice2.sender = "Suraj";
+// notice2.date = new Date(2013,1,2);
+// notice2.subject = "First Use";
+// notice2.message = "I love this notice Board";
+// repository.push(notice2);
+
+/* --- Prep ---- */
+
+var displayNotice = function(request, response){
+  repository = JSON.parse(fs.readFileSync("notices.txt"));
+  response.write("Hello World");
+ //  repository.forEach(function(notice){
+ //    response.write(JSON.stringify(notice));
+ //  })
+	repository.forEach(function(notice){
+    response.write("Author : " + notice.sender + '\n');
+    response.write("Date : " + notice.date + '\n');
+    response.write("Subject : " + notice.subject + '\n');
+    response.write(notice.message + '\n\n');
+  });
+}
+var path = "see";
+var server = function(request, response){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  if(path == "see"){
+    displayNotice(request,response);
+  }
+  response.end();
 }
 
-http.createServer(showOutput).listen(8282);
+http.createServer(server).listen(8085);
