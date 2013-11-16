049fd48c71a29d31da962ceb2903c4b5f2900b4d
unknown <surajbab@INSURAJBAB.corporate.thoughtworks.com>
2013-11-16 13:45:05 +0530

    wirking sign up ,log in and other facilities

--- request_handler.js
+++ request_handler.js
@@ -14,31 +14,15 @@ var getNoticeFromURL = function(req,res){
     if(notice.sender && notice.subject && notice.message && notice.date)
 	    return notice;
     return 0;
-}
-var handler = {};
-	handler['/'] = function(req,res){
-	res.writeHead(200, {"Content-Type":contentType.html});
-	var notice = getNoticeFromURL(req,res);
-	notice && addNewNotice(res,notice);
-	res.write(pages.home);
-	res.end();
 };
+
 var addNewNotice = function(res,notice){
     pages.noticeBoard.push(notice);
     fs.writeFile('./notice_files/dataBase/notices.json',JSON.stringify(pages.noticeBoard));
     res.write('<a href = "/see">Notice added. Click to see the NOTICE BOARD</a>');
     res.end();
-}
-handler['/login'] = function(req,res){
-    userDetails = url.parse(req.url,true).query;
-    var user = {};
-    var options = {hostname: 'localhost', port: 8085,};
-    user.id = userDetails.userID;
-    user.passwd = userDetails.passwd;
-    res.write('<a href = "/see">Notice added. Click to see the NOTICE BOARD</a>');
-    (pages.loginDetails[user.id] && pages.loginDetails[user.id].passwd == user.passwd) && (home = home.replace(/none/g,'block'))&&(home = home.replace(/block/,'none')) || console.log("hi m here");
-    handler['/'](req,res);
 };
+var handler = {};
 handler['/post'] = function(req,res){
     res.writeHead(200, {"Content-Type":contentType.html});
 	res.write(pages.addNotice_html);
@@ -61,10 +45,17 @@ handler['/see'] = function(req,res){
     res.write(pages.viewnotice.replace(/{TEXT}/,notices.join('<br/>')));
   	res.end();
 };
-handler['/images/bg.jpg'] = function(req,res){
-	res.writeHead(200,{'Content-Type': contentType.jpg});
-    res.write(pages.bg_jpg); 
-  	res.end();	
+handler['/login'] = function(req,res){
+    userDetails = url.parse(req.url,true).query;
+    var options = {hostname: 'localhost', port: 8085,};
+    var user = {};
+    user.id = userDetails.userID;
+    user.password = userDetails.password;
+    pages.loginDetails[user.id] && 
+        (pages.loginDetails[user.id].password == user.password) && 
+        (pages.home = pages.home.replace(/none/g,'block'))&&
+        (pages.home = pages.home.replace(/block/,'none')) || console.log("hi m here");
+    handler['/'](req,res);
 };
 handler['/signUp'] = function(req,res){
     res.writeHead(200, {"Content-Type":"text/html"});
@@ -74,7 +65,7 @@ handler['/signUp'] = function(req,res){
 handler['/signUpInfo'] = function(req,res){
     var errorMsg="Pas<div id=\"reg_error_inner\" class=\"_58mo\">";
     var errorlength = errorMsg.length+37;
-    var  userInfo= {};
+    var  userInfo = {};
     var address = url.parse(req.url,true);
     (address.query.Npwd!=address.query.Cpwd)&& (errorMsg += " password ");
     (address.query.Email!=address.query.cEmail)&& (errorMsg += " Email Address ");
@@ -83,19 +74,35 @@ handler['/signUpInfo'] = function(req,res){
     if(errorlength<errorMsg.length)   
         res.write(pages.signUp.replace(/{Error}/,errorMsg));
     else{
-        userInfo.uName = address.query.uName;
-        userInfo.PassWord = address.query.Npwd;
-        userInfo.Email = address.query.Email;
+        userID = address.query.uName;
+        userInfo[userID] = {};
+        userInfo[userID].password = address.query.Npwd;
+        userInfo[userID].email = address.query.Email;
+
         pages.loginDetails.push(userInfo);
         fs.writeFile('./notice_files/dataBase/loginDetails.json',JSON.stringify(pages.loginDetails));      
-        res.write("working");   
+        res.write(pages.home);   
     }
     res.end();
     return 0;
 };
 handler['/logout'] = function(req,res){
-    (home = home.replace(/block/g,'none'))&&(home = home.replace(/none/,'block'));
-    res.write(home);
+    (pages.home = pages.home.replace(/block/g,'none'))&&(pages.home = pages.home.replace(/none/,'block'));
+    res.write(pages.home);
     res.end();
 }
+
+handler['/images/bg.jpg'] = function(req,res){
+	res.writeHead(200,{'Content-Type': contentType.jpg});
+    res.write(pages.bg_jpg); 
+  	res.end();	
+};
+handler['/'] = function(req,res){
+    res.writeHead(200, {"Content-Type":contentType.html});
+    var notice = getNoticeFromURL(req,res);
+    notice && addNewNotice(res,notice);
+    res.write(pages.home);
+    res.end();
+};
+
 exports.path = handler;
